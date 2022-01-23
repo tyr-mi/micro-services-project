65e208e72b2e7dd46eb8d610550b0f514104114c
amian <a.mianji.am@gmail.com>
2022-01-23 14:38:16 +0330

    inital commit

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbount POST # Command Service");
+
+            return Ok("Inbound test of from platforms controller");
+        }
+
+    }
+}
